9f1bd4947f5835e29ae7644ed934b7e693269fae
Mariana <mariana@Marianas-MacBook-Pro.local>
2015-05-03 22:15:55 +0200

    Initial Commit

---
+++ PersonalAssistant/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PersonalAssistant
+//
+//  Created by Mariana on 5/3/15.
+//  Copyright (c) 2015 Mariana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
